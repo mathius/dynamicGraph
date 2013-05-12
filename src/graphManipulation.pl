761b0dafa2d8921e21c1b6576a1739bb3209752d
Martin Ukrop <mukrop@mail.muni.cz>
2013-05-12 16:42:52 +0200

    documentation, graphMoment generation
    - documentation added
    - graphName made private but accessible
    - new module with graphMoment predicate
    - retract converted to retractall

---
+++ src/graphManipulation.pl
@@ -0,0 +1,43 @@
+% DynamicGraph, graphManipulation module
+%
+% Module basic graph manipulations:
+% - load graph for the moment to database
+% - depth first search in this graph
+%
+% date: 2013-05-12
+% responsible for exported functions:
+%       Martin Ukrop (graphInMoment/1, edge/2)
+%
+:- module( graphManipulation, [graphInMoment/1, edge/2] ).
+
+:- use_module( graph, [edge/2] )
+
+/* edge( +-Source, +-Destionation )
+dynamic predicate representing edge in graph in given moment
+*/
+:- dynamic edge/2.
+
+/* graphInMoment( +TimeMoment )
+loads edges for the required moment from the currently loaded graph
+edges are saved as edge/2 predicates into the database
+always succeeds exactly once
+@param +TimeMoment      desired time moment (dynamicGraph format)
+*/
+graphInMoment( TimeMoment ) :-
+    deleteGraphMoment,
+    (   edge( Source, Destionation, BeginTime, EndTime),
+        BeginTime =< TimeMoment,
+        EndTime >= TimeMoment,
+        assertz( edge( Source, Destination ) ),
+        fail
+    ;
+        true
+    ),
+    !.
+
+/* deleteGraphMoment/0
+retract all edge/2 predicates currently in database
+*/
+deteleGraphMoment :-
+    retractall( edge( _Source, _Destination) ).
+    
