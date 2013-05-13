c42be33949c0b7eb0f9f75cd710be55eebc3a49a
Vladimir Still <vl.still@gmail.com>
2013-05-13 19:38:46 +0200

    graphAtTime: Clean up and fix some minor issues.

--- src/graphAtTime.pl
+++ src/graphAtTime.pl
@@ -4,9 +4,9 @@
 %
 % date: 2013-05-13
 % responsible for exported functions:
-%       Vladimír Štill ( initialize/0, edgeNow/2, advanceMinute/1 )
+%       Vladimír Štill ( initialize/1, edgeNow/2, advanceMinute/1 )
 %
-:- module( graphAtTime, [ initialize/0, edgeNow/2, advanceMinute/1 ] ).
+:- module( graphAtTime, [ initialize/1, edgeNow/2, advanceMinute/1 ] ).
 
 :- use_module( utilities, [ concatenateAtoms/2, numberToAtom/2 ] ).
 :- use_module( changeList, [ getChangeList/1 ] ).
@@ -18,7 +18,7 @@
 * @From
 * @To
 */
-edgeNow( From, To ) :- e( From, To ).
+edgeNow( From, To ) :- e( From, To ), ! ; e( To, From ).
 
 /* advanceMinute( -NextMinute )
 * 
@@ -29,14 +29,14 @@ advanceMinute( NextMinute ) :-
     , !
     , advanceMinute( Changes, NextMinute ).
 
-/* initialize
+/* initialize( -InitialTime )
 */
-initialize :- 
+initialize( InitialTime ) :- 
       retractGraph
     , getChangeList( ChangeList )
     , assertz( ChangeList )
     , !
-    , makeInitialGraph( ChangeList ).
+    , makeInitialGraph( InitialTime, ChangeList ).
 
 retractGraph :-
       retractall( changeList( _, _, _ ) )
@@ -47,12 +47,12 @@ retractGraph :-
     , retractall( e( _, _ ) )
     , !.
 
-makeInitialGraph( changeList( StartOfTime, EndOfTime, Changes ) ) :-
+makeInitialGraph( InitialTime, changeList( StartOfTime, EndOfTime, Changes ) ) :-
       assertz( startOfTime( StartOfTime ) )
     , assertz( endOfTime( EndOfTime ) )
     , !
-    , Changes = [ minute( M, NowChanges ) | NextChanges ]
-    , assertz( currentTime( M ) )
+    , Changes = [ minute( InitialTime, NowChanges ) | NextChanges ]
+    , assertz( currentTime( InitialTime ) )
     , assertz( changesTail( NextChanges ) )
     , !
     , makeMinute( NowChanges ).
@@ -67,7 +67,8 @@ makeMinute( [ deleteEdge( X, Y ) | XS ] ) :-
     , !
     , makeMinute( XS ).
 
-advanceMinute( [], EndOfTime ) :- endOfTime( EndOfTime ).
+% no end case -- advanceMinute( [], EndOfTime ) -- as last minute
+% is generated when list is singleton
 advanceMinute( [ minute( Minute, NowChanges ) | NextChanges ], Minute ) :-
       retractall( currentTime( _ ) )
     , assertz( currentTime( Minute ) )
